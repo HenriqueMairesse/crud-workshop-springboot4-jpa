7417ddf33dbcc94005148d89bd7303767df4f168
MImortal <hvmkiki@gmail.com>
2026-02-22 10:20:18 -0300

    Project created

---
+++ src/main/java/com/midev/coursespringbootjpa/CoursespringbootjpaApplication.java
@@ -0,0 +1,13 @@
+package com.midev.coursespringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringbootjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringbootjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midev/coursespringbootjpa/CoursespringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midev.coursespringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringbootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
