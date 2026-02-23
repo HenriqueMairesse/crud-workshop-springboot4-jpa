0aeae2bfbff7bb4b0eef89ec2108fc65d22be646
MImortal <hvmkiki@gmail.com>
2026-02-23 14:20:50 -0300

    Exception handling - findById

--- src/main/java/com/midev/coursespringbootjpa/services/UserService.java
+++ src/main/java/com/midev/coursespringbootjpa/services/UserService.java
@@ -2,8 +2,10 @@ package com.midev.coursespringbootjpa.services;
 
 import com.midev.coursespringbootjpa.entities.User;
 import com.midev.coursespringbootjpa.repositories.UserRepository;
+import com.midev.coursespringbootjpa.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
